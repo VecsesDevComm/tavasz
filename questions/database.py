a0b9bf13b77bfc9376b04919fa1c53901e498d44
Lacikati <lacikakatika28@gmail.com>
2019-03-03 16:43:54 +0100

    fix get_questions

--- questions/database.py
+++ questions/database.py
@@ -1,2 +1,7 @@
+import sqlite3
+conn = sqlite3connect('questions.db')
+
 def get_questions():
-  return 'yooo'
+  cur=.conn.cursor()
+  res=cur.execute('select*from questions')
+  return res.fetchall()
